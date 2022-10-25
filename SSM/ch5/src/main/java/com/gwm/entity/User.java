abc73998a01eb245b5955933935ac411078b011f
GW00307172 <gw00307172@tech.gwm.cn>
2022-10-25 16:15:17 +0800

    Spring数据库基本操作 jdbcTemplate的基本用法

---
+++ SSM/ch5/src/main/java/com/gwm/entity/User.java
@@ -0,0 +1,53 @@
+package com.gwm.entity;
+
+
+public class User {
+    private Integer id;
+    private String name;
+    private String sex;
+
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", number=" + number  +
+                '}';
+    }
+}
