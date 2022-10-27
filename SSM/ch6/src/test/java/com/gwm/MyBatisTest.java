0381c72341245153edb9ab341a0751c95352b7f3
GW00307172 <gw00307172@tech.gwm.cn>
2022-10-27 13:35:45 +0800

    Mybatis半自动映射（Sql映射文件）

--- SSM/ch6/src/test/java/com/gwm/MyBatisTest.java
+++ SSM/ch6/src/test/java/com/gwm/MyBatisTest.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import javax.annotation.Resource;
 import java.io.InputStream;
 import java.util.List;
 
@@ -20,26 +19,37 @@ public class MyBatisTest {
             SqlSession ss = ssf.openSession();
             //SqlSession 执行映射文件中定义的sql，并返回映射结果
 
-            //查
+
+
+            //数据库增删改查的基本操作   看数据库是怎么设计的   改的时候是通过什么对应的id还是number
+ /*           //通过id去查User
             User u = ss.selectOne("com.gwm.mapper.UserMapper.selectUserById",1);
             System.out.println(u);
-            //增
+            //直接增加User对象
             User user = new User();
+            user.setId(19);
             user.setName("wangxiaoming");
             user.setSex("女");
-            user.setNumber("183206255");
+            user.setNumber("183");
             ss.insert("com.gwm.mapper.UserMapper.addUser",user);
-            //改
+            //通过id去修改一条数据  在sql语句中有体现
             user.setNumber("183206222");
+            System.out.println("这个时候的User"+user);
             ss.update("com.gwm.mapper.UserMapper.updateUser",user);
             //删
-            ss.delete("com.gwm.mapper.UserMapper.deleteUser",user);
-
+            ss.delete("com.gwm.mapper.UserMapper.deleteUser",user.getId());
+*/
             //查询所有的User
-            List<User> list = ss.selectList("com.gwm.UserMapper.selectAllUser");
+            List<User> list = ss.selectList("com.gwm.mapper.UserMapper.selectAllUser");
+            //System.out.println(list);
             for(Object euser :list) {
                 System.out.println(euser);
             }
+
+            //提交事务
+            ss.commit();
+            //关闭事务
+            ss.close();
         }catch (Exception e){
             e.printStackTrace();
         }
