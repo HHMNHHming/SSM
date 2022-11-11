f792afb371e8255435bb7710b17cb3f9d2c4140f
GW00307172 <gw00307172@tech.gwm.cn>
2022-11-11 11:26:05 +0800

    Spring-Mybatis整合  oneToOne

--- ch7/src/main/java/com/gwm/dao/PersonDao.java
+++ ch7/src/main/java/com/gwm/dao/PersonDao.java
@@ -4,9 +4,10 @@ import com.gwm.po.Person;
 import com.gwm.pojo.SelectPersonById;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
-//spring的注解repository
-@Repository("personDao")
+//Spring的注解repository
+//@Repository("personDao")
 //用于生成dao实现类的Mapper注解
+   //这句话非常的重要！！！  这里的dao接口，是通过mybatis注入到Spring容器来管理的，而不是注解。
 @Mapper
 public interface PersonDao {
     //使用嵌套查询，会执行两个sql语句
